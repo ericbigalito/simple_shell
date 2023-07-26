3c4564a6b9afe231e0fb6f7a4edcc9f5c9c043b5
Eric Mugambi <emugarz@gmail.com>
2023-07-26 22:36:33 +0300

    Update

--- errors.c
+++ errors.c
@@ -2,7 +2,6 @@
 
 /**
  *_eputs - prints an input string
- * @str: the string to be printed
  *
  * Return: Nothing
  */
@@ -21,7 +20,6 @@ void _eputs(char *str)
 
 /**
  * _eputchar - writes the character c to stderr
- * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -43,8 +41,6 @@ int _eputchar(char c)
 
 /**
  * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -66,8 +62,6 @@ int _putfd(char c, int fd)
 
 /**
  *_putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
  *
  * Return: the number of chars put
  */
