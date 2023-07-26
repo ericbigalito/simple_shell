6cf81229dfcdb208479e35a641594493a5596145
Eric Mugambi <emugarz@gmail.com>
2023-07-27 01:43:03 +0300

    Update

--- lists.c
+++ lists.c
@@ -2,9 +2,6 @@
 
 /**
  * add_node - adds a node to the start of the list
- * @head: address of pointer to head node
- * @str: str field of node
- * @num: node index used by history
  *
  * Return: size of list
  */
@@ -35,9 +32,6 @@ list_t *add_node(list_t **head, const char *str, int num)
 
 /**
  * add_node_end - adds a node to the end of the list
- * @head: address of pointer to head node
- * @str: str field of node
- * @num: node index used by history
  *
  * Return: size of list
  */
@@ -96,8 +90,6 @@ size_t print_list_str(const list_t *h)
 
 /**
  * delete_node_at_index - deletes node at given index
- * @head: address of pointer to first node
- * @index: index of node to delete
  *
  * Return: 1 on success, 0 on failure
  */
@@ -136,7 +128,6 @@ int delete_node_at_index(list_t **head, unsigned int index)
 
 /**
  * free_list - frees all nodes of a list
- * @head_ptr: address of pointer to head node
  *
  * Return: void
  */
