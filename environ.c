389509f4b75719664b9913610d8815a9cf9294e6
Eric Mugambi <emugarz@gmail.com>
2023-07-26 22:32:36 +0300

    Update

--- environ.c
+++ environ.c
@@ -2,8 +2,6 @@
 
 /**
  * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
  * Return: Always 0
  */
 int _myenv(info_t *info)
@@ -14,8 +12,6 @@ int _myenv(info_t *info)
 
 /**
  * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
  *
  * Return: the value
  */
@@ -37,8 +33,6 @@ char *_getenv(info_t *info, const char *name)
 /**
  * _mysetenv - Initialize a new environment variable,
  *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
  *  Return: Always 0
  */
 int _mysetenv(info_t *info)
@@ -55,8 +49,6 @@ int _mysetenv(info_t *info)
 
 /**
  * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
  *  Return: Always 0
  */
 int _myunsetenv(info_t *info)
@@ -76,8 +68,6 @@ int _myunsetenv(info_t *info)
 
 /**
  * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
  * Return: Always 0
  */
 int populate_env_list(info_t *info)
